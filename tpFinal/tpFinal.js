62f1cb877c46ecda088524ec6145b5cb23ad92aa
Luzaru07 <luzcalderonoficial@gmail.com>
2025-10-01 12:14:14 -0300

    Add files via upload

--- tpFinal/tpFinal.js
+++ tpFinal/tpFinal.js
@@ -1,12 +1,11 @@
 let pantalla = 0; // 0 = inicio, 1 = juego, 2 = créditos, 3 = fin
-let img1;
-let img2;
-let img3;
+let img1, img2, img3,img4;
 
 function preload () {
   img1 = loadImage ("data/imagen1.jpg");
   img2 = loadImage ("data/imagen2.jpg");
   img3 = loadImage ("data/imagen3.jpg");
+  img4 = loadImage ("data/imagen4.jpg");
 }
 
 
@@ -24,33 +23,41 @@ function draw() {
   if (pantalla === 0) {
     // Pantalla de inicio
     background(50, 100, 200);
-    image (img1, 0, 0, width, height);
     fill(255);
     text("Pantalla 0: Inicio", width/2, height/2);
-    textSize(16);
+    textSize(25);
     text("Haz clic para ir a la siguiente", width/2, height/2 + 40);
     
   } else if (pantalla === 1) {
     // Pantalla del juego
     background(200, 50, 50);
-    image (img2, 0, 0, width, height);
+    image (img1, 0, 0, width, height);
     fill(255);
-    textSize(32);
-    text("Pantalla 1: Juego", width/2, height/2);
+    textSize(28);
+    text("Pantalla 1: Encuentro con Alecto", width/2, height/2);
     
   } else if (pantalla === 2) {
     // Pantalla de créditos
     background(50, 200, 100);
-    image (img3, 0, 0, width, height);
+    image (img2, 0, 0, width, height);
     fill(0);
-    text("Pantalla 2: Créditos", width/2, height/2);
+    text("Pantalla 2: Desafio, Aceptar o Rechazar", width/2, height/2);
     
   } else if (pantalla === 3) {
     // Pantalla de fin
     background(0);
+    image (img3, 0, 0, width, height);
     fill(255);
-    text("Pantalla 3: Fin", width/2, height/2);
-    textSize(16);
+    text("Pantalla 3: Rechaza y pelea", width/2, height/2);
+    textSize(25);
+    text("Haz clic para volver al inicio", width/2, height/2 + 40);
+  } else if (pantalla === 4) {
+    // Pantalla de fin
+    background(0);
+    image (img4, 0, 0, width, height);
+    fill(255);
+    text("Pantalla 4: Fin", width/2, height/2);
+    textSize(25);
     text("Haz clic para volver al inicio", width/2, height/2 + 40);
   }
 }
