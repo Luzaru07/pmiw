791b2fcaf2bdb9489441eb5644a23a4be2f4baf5
Luzaru07 <luzcalderonoficial@gmail.com>
2025-10-11 22:44:51 -0300

    Add files via upload

--- tpFinal/funciones.js
+++ tpFinal/funciones.js
@@ -1,8 +1,37 @@
 function mousePressed () {
-  // Avanzar pantalla al hacer clic
-  pantalla++;
-  if (pantalla > 7) {
-    pantalla = 0; // vuelve al inicio
+  if (mouseX, mouseY) {
+    sonidoClick.play(); //sonido al dar click
+  }
+  if (dist (mouseX, mouseY, 60, 40) <30) {
+    //solo si quiero que suene cuando
+    // clickeo el botón de sonido:
+    //sonidoClick.play();
+
+    //Boton de sonido:
+    if ( sonidoAmbiente.isPlaying() ) {
+      sonidoAmbiente.pause();
+    } else {
+      sonidoAmbiente.loop();
+    }
+  }
+  
+  
+  if (dist (mouseX, mouseY, 0, 0) <1) {
+    pantalla++;
+    if (pantalla > 8) {
+      pantalla = 0; //vuelve al inicio
+    }
+  }
+  
+  
+    // Botón "Anterior"
+  if (mouseX > 20 && mouseX < 120 && mouseY > height - 60 && mouseY < height - 20) {
+    pantalla = max(0, pantalla - 1);
+  }
+
+  // Botón "Siguiente"
+  if (mouseX > width - 120 && mouseX < width - 20 && mouseY > height - 60 && mouseY < height - 20) {
+    pantalla = min(8, pantalla + 1);
   }
 }
 
@@ -10,64 +39,69 @@ function pantallaInicio() {
   push();
   image (img[0], 0, 0, width, height);
   fill(255);
-  text("Pantalla 0: Inicio", width/2, height/2);
   textSize(25);
-  text("Haz clic para empezar", width/2, height/2 + 40);
+  text("INICIO", width/2, height/2);
   pop();
 }
-
 function pantalla1() {
   push();
   image (img[1], 0, 0, width, height);
   fill(255);
-  textSize(28);
-  text("Pantalla 1: Encuentro con Alecto", width/2, height/2);
+  textSize(18);
+  text("Kratos se encuentra solo...", width/2, 40);
   pop();
 }
-
 function pantalla2() {
   push();
   image (img[2], 0, 0, width, height);
-  fill(255);
-  text("Pantalla 2: Desafio, Aceptar o Rechazar", width/2, height/2);
+  fill(13, 95, 255);
+  textSize(18);
+  text("Hola... kratos...", 160, 150);
+  text("fantasma de esparta,", 160, 170);
+  text("tengo una oferta para ti", 165, 190);
   pop();
 }
-
 function pantalla3() {
   push();
   image (img[3], 0, 0, width, height);
   fill(255);
-  text("Pantalla 3: Rechaza y pelea", width/2, height/2);
-  textSize(25);
-  text("Haz clic para continuar", width/2, height/2 + 40);
+  text("te concedo el  honor", width/2 + 100, 100);
+  text("de convertirte en mi siervo", width/2 + 100, 100);
+  text("de convertirte en mi siervo", width/2 + 100, 100);
   pop();
 }
-
 function pantalla4() {
   push();
   image (img[4], 0, 0, width, height);
-  fill(0);
-  text("Rechaza y pelea", width/2, height/2);
-  textSize(25);
-  text("Haz clic para continuar", width/2, height/2 + 40);
+  fill(255);
+  text("Al aceptar se convierte en su siervo", width/2, 40);
   pop();
 }
-
 function pantalla5() {
   push();
+  image (img[5], 0, 0, width, height);
   fill(255);
-  text("pantalla 5", width/2, height/2);
   textSize(20);
+  text("Al rechazar se enfrenta a una pelea con Alecto y los lobos", width/2 + 50, 40);
   pop();
 }
 function pantalla6() {
   push();
+  image (img[6], 0, 0, width, height);
+  fill(255);
+  text("Pelea por su vida", width/2, 40);
+  textSize(20);
+  pop();
+}
+function pantalla7() {
+  push();
+  image (img[7], 0, 0, width, height);
   fill(255);
   text("pantalla 6", width/2, height/2);
   textSize(20);
   pop();
 }
-function pantallaCréditos() {
+function pantallaCreditos() {
   push();
   fill(0);
   textSize(35);
@@ -77,7 +111,22 @@ function pantallaCréditos() {
   textSize(20);
   text("Producido por:", width/2, height/2 + 30);
   textSize(23);
-  text("Luz Rodríguez Calderón (122866/2) ", width/2, height/2 + 70);
-  text("y Valentín San Román Ignacio ()", width/2, height/2 + 100);
+  text("Luz Rodríguez Calderón (47.953.614) ", width/2, height/2 + 70);
+  text("y Valentín San Román Ignacio (45.908.478)", width/2, height/2 + 100);
   pop();
+}
+
+function dibujarBotones(){
+  //Botón anterior
+    fill(180);
+  rect(20, height - 60, 100, 40, 10);
+  fill(0);
+  textSize(18);
+  text("Anterior", 70, height - 40);
+
+  // Botón siguiente
+  fill(180);
+  rect(width - 120, height - 60, 100, 40, 10);
+  fill(0);
+  text("Siguiente", width - 70, height - 40);
 }
--- tpFinal/tpFinal.js
+++ tpFinal/tpFinal.js
@@ -1,16 +1,16 @@
 let DEBUG = true; //cambiar a false al finalizar
-let pantalla = 0; // 0 = inicio, 7 = créditos
-//let img0, img1, img2, img3, img4; q quede de recuerdo...
+let pantalla = 0; // 0 = inicio, 8 = créditos
 let img = [];
+let sonidoAmbiente;
+let sonidoClick;
 
 function preload () {
-  /*
-  img0[0] = loadImage ("data/imagen0.jpg");
   //loadSound(), loadFont()...
-  */
-  for ( let i=0; i<5; i++ ) {
-  img[i] = loadImage ("./data/imagen"+i+".jpg"); //+i+ = +nf(i,2)+
+  for ( let i=0; i<10; i++ ) {
+    img[i] = loadImage ("./data/imagen"+i+".jpg"); //+i+ = +nf(i,2)+ si es más de 2 cifras
   }
+  sonidoAmbiente = loadSound("./data/sonidoAmbiente.mp3");
+  sonidoClick = loadSound("./data/sonidoClick.mp3");
 }
 
 
@@ -23,53 +23,50 @@ function setup() {
 
 function draw() {
   background(220);
-  
+
   if (pantalla === 0) {
     // Pantalla de inicio
     background(50, 100, 200);
     pantallaInicio();
-    
   } else if (pantalla === 1) {
-    // Pantalla del juego
-    background(0);
     pantalla1();
-    
   } else if (pantalla === 2) {
-    // Pantalla 
-    background(0);
     pantalla2();
-    
   } else if (pantalla === 3) {
-    // Pantalla de fin
-    background(0);
     pantalla3();
-    
   } else if (pantalla === 4) {
-    // Pantalla de fin
-    background(255);
     pantalla4();
-    
   } else if (pantalla === 5) {
-    // Pantalla de fin
-    background(148, 224, 18); //verde
     pantalla5();
-    
   } else if (pantalla === 6) {
-    // Pantalla de fin
-    background(250, 91, 141); //rosa
     pantalla6();
-    
-  } else if (pantalla === 7) { //CRÉDITOS
-    // Pantalla de fin
-    background(255, 192, 54); //naranja
-    pantallaCréditos(); 
+  } else if (pantalla === 7) {
+    pantalla7();
+  } else if (pantalla === 8) {
+    pantallaCreditos();
   }
+  
+  //Botón para activar y/o pausar la música
+  fill (207, 8, 252);
+  ellipse (60, 40, 80, 50);
+  
+  /*
+  if ( sonidoAmbiente.isPlaying() ) {
+    let paneo = map(mouseX, 0, width, -1, 1);
+    paneo = constrain (paneo, -1, 1);
+    sonidoAmbiente.pan( paneo );
+    //console.log( paneo );
+  }
+  */
+  
   if ( DEBUG ) {
-  push();
-  fill (0);
-  textAlign(LEFT); //Derecha
-  textSize(20);
-  text( "Pantalla:" + pantalla, 20, 20);
-  pop();
+    push();
+    fill (0);
+    textAlign(LEFT); //Derecha
+    textSize(20);
+    text( "Pantalla:" + pantalla, 20, 20);
+    pop();
   }
+  
+  dibujarBotones();
 }
